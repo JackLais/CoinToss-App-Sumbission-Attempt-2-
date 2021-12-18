80f657cf4917e608dea776bd15a77d46678644f4
JackLais <jlais@bsd220.org>
2021-12-18 01:13:23 -0600

    Initial Commit

---
+++ CoinToss App (Sumbission Attempt 2)/CoinToss_App__Sumbission_Attempt_2_App.swift
@@ -0,0 +1,17 @@
+//
+//  CoinToss_App__Sumbission_Attempt_2_App.swift
+//  CoinToss App (Sumbission Attempt 2)
+//
+//  Created by Student on 12/18/21.
+//
+
+import SwiftUI
+
+@main
+struct CoinToss_App__Sumbission_Attempt_2_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CoinToss App (Sumbission Attempt 2)/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CoinToss App (Sumbission Attempt 2)
+//
+//  Created by Student on 12/18/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
